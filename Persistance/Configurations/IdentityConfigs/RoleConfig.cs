2796a71e6594cde5545247e42ff916d500f59eca
CrashTagiyev <121178020+CrashTagiyev@users.noreply.github.com>
2024-08-14 01:06:00 +0400

    aa

--- Persistance/Configurations/IdentityConfigs/RoleConfig.cs
+++ Persistance/Configurations/IdentityConfigs/RoleConfig.cs
@@ -10,21 +10,25 @@ namespace Persistance.Configurations.IdentityConfigs
 		{
 			builder.HasData(new IdentityRole<int>
 			{
+				Id = 1,
 				Name = "User",
 				NormalizedName = "USER"
 			},
 			new IdentityRole<int>
 			{
+				Id = 2,
 				Name = "Vet",
 				NormalizedName = "VET"
 			},
 			new IdentityRole<int>
 			{
+				Id = 3,
 				Name = "Company",
 				NormalizedName = "COMPANY"
 			},
 			new IdentityRole<int>
 			{
+				Id = 4,
 				Name = "Admin",
 				NormalizedName = "Admin"
 			});
